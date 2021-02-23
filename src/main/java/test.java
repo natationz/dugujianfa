dea26c8b80f6ba17d8949677f9028d322a76fe20
natationz <577530562@qq.com>
2021-02-23 21:02:14 +0800

    Update test.java

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("aa");
         System.out.println("晗哥慢慢冲");
+        System.out.println("快男鑫哥");
     }
 }
