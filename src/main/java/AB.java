977f3145089948eed84136f54b6e276f37197d5e
zhaohan <577530562@qq.com>
2021-02-26 14:00:50 +0800

    update

---
+++ src/main/java/AB.java
@@ -0,0 +1,5 @@
+public class AB {
+    public static void main(String[] args) {
+        System.out.println("测速");
+    }
+}
--- src/main/java/KuihuaBaoDian.java
+++ src/main/java/KuihuaBaoDian.java
@@ -2,5 +2,6 @@ public class KuihuaBaoDian {
     public static void main(String[] args) {
         System.out.println("欲练此功");
         System.out.println("必先自宫");
+        System.out.println("修仙成功");
     }
 }
